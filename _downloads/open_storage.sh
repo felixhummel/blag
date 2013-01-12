4dc01ed55af0927569a431f14c47723263024cd1
Felix Hummel <felix@felixhummel.de>
2013-01-12 01:04:38 +0100

    auto job. see https://github.com/felixhummel/blag

--- _downloads/open_storage.sh
+++ _downloads/open_storage.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 sudo cryptsetup luksOpen /dev/md0 crypt0
+
+# wait until block device is available
+while [[ ! -b /dev/raid/storage ]]; do
+  echo -n '.'
+  sleep 0.5
+done
+echo
 sudo mount /dev/raid/storage /media/storage
 
---
+++ _sources/admin/munin.txt
@@ -0,0 +1,8 @@
+Munin
+=====
+Single box::
+
+    sudo apt-get --yes install munin munin-node munin-plugins-extra
+    ls /etc/munin/munin.conf
+    ls /var/cache/munin/www/
+
